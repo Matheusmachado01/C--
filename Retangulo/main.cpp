2e1b2ce650d21923bef35754b10dbc863bdbce7c
Matheus Machado <103153379+Matheusmachado01@users.noreply.github.com>
2022-07-02 16:06:50 -0300

    Exercicio Retangulo

---
+++ Retangulo/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+int main()
+{
+    //double base, altura, area, perimetro, diagonal;
+
+    cout << "Base do retangulo: ";
+    cin >> base;
+    cout << "Altura do retangulo: ";
+    cin >> altura;
+
+    area = base * altura;
+    perimetro = 2 * (base + altura);
+    diagonal = sqrt (pow(base, 2.0) + pow (altura, 2.0));
+
+    cout << fixed << setprecision(4);
+    cout << "AREA = " << area << endl;
+    cout << "PERIMETRO = " << perimetro << endl;
+    cout << "DIAGONAL = " << diagonal << endl;
+
+    return 0;
+}
